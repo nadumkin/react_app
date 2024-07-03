6e0c12fbd38ea521456fab39184067a76b70e31b
Nikita Dumkin <ndumkin@mail.ru>
2024-07-03 12:03:11 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import ButtonGroup from "./components/ButtonGroup/ButtonGroup";
+import {Display} from "./components/Display/Display";
 
 function App() {
 
 
   return (
     <div className="App">
+        <Display />
       <ButtonGroup />
     </div>
   );
--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import './style.css'
 
-export const Display = () => {
+export const Display = ({inputString}) => {
 
+    const [displayState, setDisplayState] = useState('0');
+
+    useEffect(() => {
+        setDisplayState(inputString);
+    }, [inputString])
 
     return (
-        <div>
-
+        <div className='displayContainer'>
+            <p>
+                {displayState}
+            </p>
         </div>
     );
 };
